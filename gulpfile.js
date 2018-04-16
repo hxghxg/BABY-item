20f05a6fd7d060de94cd989c131315def723f4b5
hxg <997927265@qq.com>
2018-04-16 12:57:25 +0800

    项目修改

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,91 @@
-/* 
-* @Author: Marte
-* @Date:   2018-04-09 09:14:06
-* @Last Modified by:   Marte
-* @Last Modified time: 2018-04-09 13:55:15
-*/
+// 在nodejs环境下运行的js文件
+// 按照nodejs的语法使用
 
-// 引入gulp
-// 模块：函数|对象
+// 引用模块: require();  得到一个对象/函数
+// gulp,gulp-sass
 
-// 印象本地安装的gulp
-var gulp = require('gulp');//得到一个对象
-var sass = require('gulp-sass');
+let gulp = require('gulp');//{task(),}
+let sass = require('gulp-sass');//fn
 
 
-// 编译sass文件
-// gulp的使用
-// 任务：
-// .. ：上层目录
-// .  ：当前目录
-
-
-// 创建一个gulp任务：编译sass
+// sass->css
 gulp.task('compileSass',function(){
-    // 查找sass文件
-    gulp.src('./src/sass/*.scss')   //得到文件流（文件在内存中的状态）scss
+    // 先查找sass文件所在目录
+    gulp.src('./src/sass/*.scss') // 返回文件流（液体，文件在内存中的状态）
 
-    // 通过管道传输
-    // 编译
-    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))                        //编译后的文件流 css
-
+    // scss->css
+    .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
 
     // 输出到硬盘
     .pipe(gulp.dest('./src/css/'))
 });
 
 
-// 创建文件监听任务：文件有修改，则自动编译
+// 监听文件修改，自动执行编译任务
 gulp.task('jtSass',function(){
-    // 当文件有修改，则执行complieSass任务
     gulp.watch('./src/sass/*.scss',['compileSass'])
+})
+
+gulp.task('default',function(){
+    // console.log(gulp)
+    gulp.src('./src/lib/bootstrap-sass-3.3.7/assets/stylesheets/*.scss')
+    .pipe(sass({outputStyle:'compact'}).on('error', sass.logError))
+    .pipe(gulp.dest('./src/css/'))
 });
 
 
-// 自动刷新页面
+// js压缩
+// let uglify = require('gulp-uglify');
+// let pump = require('pump');
+// let concat = require('gulp-concat');
+// let rename = require('gulp-rename');
+
+// gulp.task('compressJs',function(cb){
+//     // gulp.src('./src/js/**/*.js')
+
+//     // .pipe(uglify())
+
+//     // // 输出到构建目录
+//     // .pipe(gulp.dest('./dist/js/'))
+
+//     pump([
+//         gulp.src('./src/js/*.js'),
+
+//         // 合并
+//         concat('index.js'),
+//         gulp.dest('./dist/js/'),
+
+//         // 压缩
+//         uglify(),
+
+//         // 重命名
+//         rename({
+//             suffix:'.min'
+//         }),
+
+//         gulp.dest('dist/js/')
+//     ],cb );
+// });
+
+// 自动刷新服务器
 var browserSync = require('browser-sync');
 
-gulp.task('server',function(){
-    // 启动一个自动刷新的服务器
+// 静态服务器
+gulp.task('server',()=>{
     browserSync({
-        //创建一个静态服务器
-        // server:'./src',
-
-        // 指定端口
-        port:666,
+        // 服务器路径
+        // server:'./src/',
 
         // 代理服务器
-        // 用browserSync代理php服务器
-        //  * 识别php
-        //  * 自动刷新
-        proxy:'http://localhost:888',
+        proxy:'http://localhost:1802',
 
-        // 监听文件修改
-        files:['./src/css/*.css','./src/api/*.php']
+        // 端口
+        port:777,
+
+        // 监听文件修改，自动刷新
+        files:['./src/**/*.html','./src/css/*.css','./src/api/*.php']
     });
 
-    // 监听sass修改
-    gulp.watch('./src/sass/*.scss',['compileSass']);
-});
+    // 监听sass文件修改，并自动编译
+    gulp.watch('./src/sass/*.scss',['compileSass'])
+})
